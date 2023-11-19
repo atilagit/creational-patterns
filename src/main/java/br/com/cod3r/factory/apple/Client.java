17e630da88164c5710cb7fc169cc004f22d5b3bb
Atila Silva <atila__7@hotmail.com>
2023-11-19 12:46:47 -0300

    SimpleFactory - é uma abordagem mais utilizada, na qual NÃO usa-se uma interface de fábrica, e além disso em uma única fábrica é possível criar todas as variações de produtos de acordo com os parâmetros passados.

--- src/main/java/br/com/cod3r/factory/apple/Client.java
+++ src/main/java/br/com/cod3r/factory/apple/Client.java
@@ -1,11 +1,18 @@
 package br.com.cod3r.factory.apple;
 
+import br.com.cod3r.factory.apple.factory.IPhoneFactory;
+import br.com.cod3r.factory.apple.model.IPhone;
+
 public class Client {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("### Ordering an iPhone X");
+		IPhone iphone1 = IPhoneFactory.getIphone("X", "standard");
+		System.out.println(iphone1);
 
 		System.out.println("\n\n### Ordering an iPhone 11 HighEnd");
+		IPhone iPhone2 = IPhoneFactory.getIphone("11", "highEnd");
+		System.out.println(iPhone2);
 	}
 }
--- src/main/java/br/com/cod3r/factory/apple/factory/IPhoneFactory.java
+++ src/main/java/br/com/cod3r/factory/apple/factory/IPhoneFactory.java
@@ -1,6 +1,38 @@
 package br.com.cod3r.factory.apple.factory;
 
 
+import br.com.cod3r.factory.apple.model.*;
+
+import java.util.Objects;
+
 public abstract class IPhoneFactory {
-	
+
+    public static IPhone getIphone(String model, String level){
+        IPhone device = null;
+
+        if ("X".equalsIgnoreCase(model)){
+            if ("standard".equalsIgnoreCase(level)) {
+                device = new IPhoneX();
+            }
+            if ("hightEnd".equalsIgnoreCase(level)) {
+                device = new IPhoneXSMax();
+            }
+        }
+        if ("11".equalsIgnoreCase(model)){
+            if ("standard".equalsIgnoreCase(level)) {
+                device = new IPhone11();
+            }
+            if ("highEnd".equalsIgnoreCase(level)) {
+                device = new IPhone11Pro();
+            }
+        }
+
+        if (Objects.nonNull(device)) {
+            device.getHardware();
+            device.assemble();
+            device.certificates();
+            device.pack();
+        }
+        return device;
+    }
 }
